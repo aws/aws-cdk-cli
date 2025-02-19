6e3e58a7e22a277426971e069a3c113d9cc93d72
Ian Hou <45278651+iankhou@users.noreply.github.com>
2025-02-18 22:27:31 -0500

    import ordering

--- packages/@aws-cdk/toolkit-lib/test/util/aws-cdk.ts
+++ packages/@aws-cdk/toolkit-lib/test/util/aws-cdk.ts
@@ -1,12 +1,46 @@
 /* eslint-disable import/no-restricted-paths */
 
+// Node.js built-in modules
 import * as path from 'node:path';
+import * as os from 'node:os';
 
-export {
-  MockSdk, MockSdkProvider, mockCloudFormationClient, mockS3Client, mockSTSClient, setDefaultSTSMocks, restoreSdkMocksToDefault,
+// Third-party modules
+import * as cxapi from '@aws-cdk/cx-api';
+import * as fs from 'fs-extra';
+
+// Local modules
+import {
+  MockSdk,
+  MockSdkProvider,
+  mockCloudFormationClient,
+  mockS3Client,
+  mockSTSClient,
+  setDefaultSTSMocks,
+  restoreSdkMocksToDefault,
 } from '../../../../aws-cdk/test/util/mock-sdk';
-export * as os from 'node:os';
+
+import {
+  SdkProvider,
+  Bootstrapper,
+  type StringWithoutPlaceholders
+} from '../../lib/api/aws-cdk';
+
+// Re-exports
 export { path };
-export * as cxapi from '@aws-cdk/cx-api';
-export * as fs from 'fs-extra';
-export { SdkProvider, Bootstrapper, type StringWithoutPlaceholders } from '../../lib/api/aws-cdk';
+export { os };
+export { cxapi };
+export { fs };
+export {
+  MockSdk,
+  MockSdkProvider,
+  mockCloudFormationClient,
+  mockS3Client,
+  mockSTSClient,
+  setDefaultSTSMocks,
+  restoreSdkMocksToDefault,
+};
+export {
+  SdkProvider,
+  Bootstrapper,
+  type StringWithoutPlaceholders
+};
