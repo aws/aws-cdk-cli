3d93b6c1f1b161c687ea9f69844d305a3e46bc92
Kaizen Conroy <zen.conroy@gmail.com>
2025-02-20 12:03:19 -0500

    merge?

--- .projenrc.ts
+++ .projenrc.ts
@@ -1137,6 +1137,7 @@ toolkitLib.package.addField('exports', {
   './package.json': './package.json',
 });
 
+toolkitLib.postCompileTask.prependExec('ts-node scripts/gen-code-registry.ts');
 toolkitLib.postCompileTask.exec('node build-tools/bundle.mjs');
 // Smoke test built JS files
 toolkitLib.postCompileTask.exec('node ./lib/index.js >/dev/null 2>/dev/null </dev/null');
---
+++ packages/@aws-cdk/toolkit-lib/scripts/gen-code-registry.ts
@@ -0,0 +1,15 @@
+import * as fs from 'fs';
+import { CODES, CodeInfo } from '../lib/api/io/private/codes';
+
+function objectToMarkdownTable(codes: Record<string, CodeInfo>) {
+  let table = '| Code | Description | Level | Data Interface |\n';
+  table += '|------|-------------| ----- | -------------- |\n';
+  
+  Object.entries(codes).forEach(([id, code]) => {
+    table += `| ${id} | ${code.description} | ${code.level} | ${code.interface} |\n`;
+  });
+  
+  return table;
+}
+
+fs.writeFileSync('CODE_REGISTRY.md', objectToMarkdownTable(CODES));
