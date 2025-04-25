cf67e57d75189e4d448fcea58aa6ef6660b6f414
Rico Huijbers <rix0rrr@gmail.com>
2025-04-25 13:38:13 +0200

    Update docs

--- packages/@aws-cdk/yarn-cling/lib/hoisting.ts
+++ packages/@aws-cdk/yarn-cling/lib/hoisting.ts
@@ -7,14 +7,12 @@ import { iterDeps, isPackage, type PackageLockFile, type PackageLockTree } from 
  * This happens in two phases:
  *
  * 1) Move every package into the parent scope (as long as it introduces no conflicts).
- *    This step mutates the dependency tree.
+ *    Leave "moved" markers to indicate that a package used to be there and no
+ *    new package with the same name should be moved up into that location.
  * 2) Once no more packages can be moved up, clean up the tree. This step mutates the
  *    tree declarations but cannot change versions of required packages. Two cleanups:
  *    2a) Remove duplicates down the tree (same version that is inherited from above)
  *    2b) Remove useless packages that aren't depended upon by anything in that subtree.
- *        To determine whether a package is useful or useless in a tree, we record
- *        each package's original dependencies before we start messing around in the
- *        tree.
  *
  * This two-phase process replaces a proces that did move-and-delete as one step, which
  * sometimes would hoist a package into a place that was previously vacated by a conflicting
