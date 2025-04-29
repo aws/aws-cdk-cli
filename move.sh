7704f960653441e00ea367eca0f84255e2013102
Momo Kornher <kornherm@amazon.co.uk>
2025-04-29 12:39:11 +0200

    refactor(toolkit-lib): move code from tmp-toolkit-helpers

--- move.sh
+++ move.sh
@@ -3,5 +3,7 @@
 rsync -av ./packages/@aws-cdk/tmp-toolkit-helpers/test/ ./packages/@aws-cdk/toolkit-lib/test/
 rsync -av ./packages/@aws-cdk/tmp-toolkit-helpers/src/ ./packages/@aws-cdk/toolkit-lib/lib/
 
+rm -rf ./packages/@aws-cdk/tmp-toolkit-helpers/
+
 git apply rm-tmp.patch
 npx projen
