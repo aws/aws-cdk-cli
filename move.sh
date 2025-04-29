16be9ef35f117cbc9dc49d9cc70cb783c26a7e55
Momo Kornher <kornherm@amazon.co.uk>
2025-04-29 15:38:15 +0200

    no move

--- move.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -av ./packages/@aws-cdk/tmp-toolkit-helpers/test/ ./packages/@aws-cdk/toolkit-lib/test/
-rsync -av ./packages/@aws-cdk/tmp-toolkit-helpers/src/ ./packages/@aws-cdk/toolkit-lib/lib/
-
-rm -rf ./packages/@aws-cdk/tmp-toolkit-helpers/
-
-git apply rm-tmp.patch
-npx projen
