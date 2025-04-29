3fb08804f11531c4c4f61ba36b466d0741a07c69
iliapolo <eli.polonsky@gmail.com>
2025-04-29 12:36:05 +0300

    mid work

--- packages/@aws-cdk-testing/cli-integ/resources/integ.jest.config.js
+++ packages/@aws-cdk-testing/cli-integ/resources/integ.jest.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const os = require('os');
 
 const rootDir = path.resolve(__dirname, '..', 'tests', process.env.TEST_SUITE_NAME);
 
@@ -19,9 +20,38 @@ module.exports = {
   // for the lock. Which is almost never what we actually care about. Set it high.
   testTimeout: 2 * 60 * 60_000,
 
-  maxWorkers: 50,
+  maxWorkers: maxWorkers(),
   reporters: [
     "default",
     ["jest-junit", { suiteName: "jest tests", outputDirectory: "coverage" }]
   ]
 };
+
+/**
+ * Based on the machine spec, calcluate the maximum number of jest workers we can start in parallel.
+ */
+function maxWorkers() {
+
+  const totalMemoryMB = os.totalmem() / 1024 / 1024;
+  const totalCores = os.cpus().length;
+
+  // empirically observed. this includes:
+  // - 150 jest test process
+  // - 150 cli subprocess
+  // - 100 app synthesis subprocess  
+  const observedWorkerMemoryMB = 300;
+
+  // leave 3GB for the OS and other external processes
+  const reservedMemoryMB = 3000;
+
+  // our processes don't take up much CPU so we allow for a large factor.
+  const cpuScaleFactor = 10;
+
+  const byMemory = Math.floor((totalMemoryMB - reservedMemoryMB) / observedWorkerMemoryMB);
+  const byCpu = cpuScaleFactor * totalCores;
+
+  const maxWorkers = Math.min(byMemory, byCpu);
+  console.log(`[integ.jest.config] calculated maxWorkers: ${maxWorkers}`)
+  return maxWorkers;
+
+}
