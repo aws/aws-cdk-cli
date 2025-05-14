e88f2b405a02379489d02b22cd96ef3fe6a97614
Rico Huijbers <rix0rrr@gmail.com>
2025-05-14 21:58:10 +0200

    REVERTME: add debug line

--- packages/@aws-cdk/toolkit-lib/lib/actions/diff/private/helpers.ts
+++ packages/@aws-cdk/toolkit-lib/lib/actions/diff/private/helpers.ts
@@ -132,6 +132,7 @@ async function changeSetDiff(
   }
 
   if (stackExists) {
+    await ioHelper.notify(IO.DEFAULT_TOOLKIT_DEBUG.msg(`Doing a changeset diff with importExistingResources: ${importExistingResources}\n`));
     return cfnApi.createDiffChangeSet(ioHelper, {
       stack,
       uuid: uuid.v4(),
