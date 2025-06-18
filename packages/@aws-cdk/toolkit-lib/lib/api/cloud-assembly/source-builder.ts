92192d1aefbb5dc6fa8f24f75233e02c30caceba
corymhall <43035978+corymhall@users.noreply.github.com>
2025-06-18 05:51:19 -0400

    fixing build error

--- packages/@aws-cdk/toolkit-lib/lib/api/cloud-assembly/source-builder.ts
+++ packages/@aws-cdk/toolkit-lib/lib/api/cloud-assembly/source-builder.ts
@@ -400,10 +400,11 @@ export abstract class CloudAssemblySourceBuilder {
             if (props.failOnMissingContext ?? true) {
               const missingKeysSet = missingContextKeys(assembly.cloudAssembly.manifest.missing);
               const missingKeys = Array.from(missingKeysSet);
-              throw new AssemblyError(
+              throw AssemblyError.withCause(
                 'Assembly contains missing context. ' +
                   "Make sure all necessary context is already in 'cdk.context.json' by running 'cdk synth' on a machine with sufficient AWS credentials and committing the result. " +
                   `Missing context keys: '${missingKeys.join(', ')}'`,
+                'Error producing assembly',
               );
             }
           }
