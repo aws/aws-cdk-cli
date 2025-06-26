fa8e02a01b9f6366bb43f6a91dd5b7c37a75c8a0
Eli Polonsky <epolon@amazon.com>
2025-06-26 16:51:44 -0400

    mid work

---
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cdk-assets/docker-credential-cdk-assets.integtest.ts
@@ -0,0 +1,14 @@
+import { integTest, withDefaultFixture } from '../../../lib';
+
+jest.setTimeout(2 * 60 * 60_000); // Includes the time to acquire locks, worst-case single-threaded runtime
+
+integTest(
+  'docker-credential-cdk-assets can be invoked as a binary',
+  withDefaultFixture(async (fixture) => {
+    await fixture.shell(['npm', 'init', '-y']);
+    await fixture.shell(['npm', 'install', 'cdk-assets@latest']);
+
+    fixture.shell(['node', './node_modules/cdk-assets/bin/docker-credential-cdk-assets', 'get'])
+
+  }),
+);
