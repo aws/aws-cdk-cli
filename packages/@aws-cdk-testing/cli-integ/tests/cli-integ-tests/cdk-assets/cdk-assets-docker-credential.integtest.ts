081e22fedc719918a90f83b1b2f863cadecc575c
Eli Polonsky <epolon@amazon.com>
2025-06-26 20:30:40 -0400

    mid work

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cdk-assets/cdk-assets-docker-credential.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/cdk-assets/cdk-assets-docker-credential.integtest.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { GetCallerIdentityCommand } from '@aws-sdk/client-sts';
 import { integTest, withDefaultFixture } from '../../../lib';
 
 jest.setTimeout(2 * 60 * 60_000); // Includes the time to acquire locks, worst-case single-threaded runtime
@@ -8,6 +11,30 @@ integTest(
     await fixture.shell(['npm', 'init', '-y']);
     await fixture.shell(['npm', 'install', 'cdk-assets@latest']);
 
-    await fixture.shell(['node', './node_modules/cdk-assets/bin/docker-credential-cdk-assets', 'get']);
+    const caller = await fixture.aws.sts.send(new GetCallerIdentityCommand());
+    const domain = 'integ.test.domain';
+    const credsFilePath = path.join(fixture.integTestDir, 'cdk-docker-creds.json');
+
+    fs.writeFileSync(credsFilePath, JSON.stringify({
+      version: '1.0',
+      domainCredentials: {
+        [domain]: {
+          ecrRepository: true,
+          roleArn: caller.Arn,
+        },
+      },
+    }));
+
+    const input = path.join(fixture.integTestDir, 'input.txt');
+    fs.writeFileSync(input, `${domain}\n`);
+
+    await fixture.shell(['node', './node_modules/cdk-assets/bin/docker-credential-cdk-assets', 'get'], {
+      modEnv: {
+        CDK_DOCKER_CREDS_FILE: credsFilePath,
+        AWS_REGION: fixture.aws.region,
+      },
+      // inhereting stdin because we need to write to it
+      stdio: [fs.openSync(input, 'r'), 'pipe', 'pipe'],
+    });
   }),
 );
