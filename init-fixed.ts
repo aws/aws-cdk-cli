802ec53eb525b3ee22da126e9772a761e907d834
Rohan Gupta <rupta@amazon.com>
2025-06-30 15:52:52 -0400

    cleaned up extraneous files (custom templates for testing purposes, etc)

--- init-fixed.ts
+++
@@ -1,8 +0,0 @@
-// Fixed version of the NPM template section with language auto-detection
-
-        template = await InitTemplate.fromPath(templatePath, templateName);
-
-        // Auto-detect language if not specified
-        if (!language && template.languages.length === 1) {
-          language = template.languages[0];
-        }
