da64d2bfcdaac94eb3c6944e2b981c28ee36c39a
Kaizen Conroy <zen.conroy@gmail.com>
2025-07-02 19:36:13 -0400

    library-version tests

--- packages/aws-cdk/lib/cli/telemetry/library-version.ts
+++ packages/aws-cdk/lib/cli/telemetry/library-version.ts
@@ -17,6 +17,11 @@ export async function getLibraryVersion(ioHelper: IoHelper): Promise<string | un
 
     const pathToPackageJson = path.join(path.dirname(stdout), 'package.json');
     const packageJson = fs.readJSONSync(pathToPackageJson);
+    if (!packageJson.version) {
+      ioHelper.defaults.trace('Could not get CDK Library Version: package.json does not have verision field');
+      return;
+    };
+  
     return packageJson.version;
   } catch (e: any) {
     ioHelper.defaults.trace(`Could not get CDK Library Version: ${e}`);
