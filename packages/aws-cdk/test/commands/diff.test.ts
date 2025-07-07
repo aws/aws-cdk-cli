541eeb43b7372c83e32b75a0e9983b35350f511c
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2025-07-07 17:17:38 -0400

    Update packages/aws-cdk/test/commands/diff.test.ts

--- packages/aws-cdk/test/commands/diff.test.ts
+++ packages/aws-cdk/test/commands/diff.test.ts
@@ -560,7 +560,6 @@ describe('non-nested stacks', () => {
       configuration: cloudExecutable.configuration,
       sdkProvider: cloudExecutable.sdkProvider,
     });
-    notifySpy.mockClear();
 
     // WHEN
     const exitCode = await toolkit.diff({
