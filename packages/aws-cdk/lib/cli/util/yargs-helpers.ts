4ac9ee26f9a568ec1c3d231a25c8daf2470e5980
Kaizen Conroy <zen.conroy@gmail.com>
2025-07-11 10:49:31 -0400

    refactor(cli): split version functions into those that display and those that do not

--- packages/aws-cdk/lib/cli/util/yargs-helpers.ts
+++ packages/aws-cdk/lib/cli/util/yargs-helpers.ts
@@ -31,7 +31,7 @@ export function yargsNegativeAlias<T extends { [x in S | L]: boolean | undefined
  * @returns the current version of the CLI
  */
 export function cliVersion(): string {
-  return version.displayVersion();
+  return version.version();
 }
 
 /**
