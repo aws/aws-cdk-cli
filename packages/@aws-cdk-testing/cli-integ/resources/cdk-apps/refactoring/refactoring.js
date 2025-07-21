d2bda95d9cb700b3538a9da535c3cc8b78c9f857
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-07-21 14:39:57 +0100

    More integ tests

--- packages/@aws-cdk-testing/cli-integ/resources/cdk-apps/refactoring/refactoring.js
+++ packages/@aws-cdk-testing/cli-integ/resources/cdk-apps/refactoring/refactoring.js
@@ -1,18 +1,31 @@
 const cdk = require('aws-cdk-lib');
 const sqs = require('aws-cdk-lib/aws-sqs');
-
-class BasicStack extends cdk.Stack {
-  constructor(parent, id, props) {
-    super(parent, id, props);
-    new sqs.Queue(this, props.queueName);
-  }
-}
+const lambda = require('aws-cdk-lib/aws-lambda');
+const s3 = require('aws-cdk-lib/aws-s3');
 
 const stackPrefix = process.env.STACK_NAME_PREFIX;
 const app = new cdk.App();
 
-new BasicStack(app, `${stackPrefix}-basic`, {
-    queueName: process.env.BASIC_QUEUE_LOGICAL_ID ?? 'BasicQueue',
+const stack  = new cdk.Stack(app, `${stackPrefix}-basic`);
+new sqs.Queue(stack, process.env.BASIC_QUEUE_LOGICAL_ID ?? 'BasicQueue');
+
+if (process.env.ADDITIONAL_QUEUE_LOGICAL_ID) {
+  new sqs.Queue(stack, process.env.ADDITIONAL_QUEUE_LOGICAL_ID);
+}
+
+// This part is to test moving a resource to a separate stack
+const bucketStack = process.env.BUCKET_IN_SEPARATE_STACK ? new cdk.Stack(app, `${stackPrefix}-bucket-stack`) : stack;
+const bucket = new s3.Bucket(bucketStack, 'Bucket');
+
+new lambda.Function(stack, 'Func', {
+  runtime: lambda.Runtime.NODEJS_22_X,
+  code: lambda.Code.fromInline(`exports.handler = handler.toString()`),
+  handler: 'index.handler',
+  environment: {
+    BUCKET: bucket.bucketName
+  }
 });
+
+
 
 app.synth();
