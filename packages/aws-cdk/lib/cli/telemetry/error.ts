25afd6f52c9519d58e2c130399db1ec3f0b928f8
Kaizen Conroy <36202692+kaizencc@users.noreply.github.com>
2025-07-22 12:27:57 -0400

    Apply suggestions from code review

--- packages/aws-cdk/lib/cli/telemetry/error.ts
+++ packages/aws-cdk/lib/cli/telemetry/error.ts
@@ -1,12 +1,14 @@
 import { ErrorName } from "./schema";
 
 export function cdkCliErrorName(name: string): ErrorName {
-  if (!isErrorName(name)) {
+  // We only record error names that we control. Errors coming from dependencies
+  // contain text that we have no control over so it is safer to not send it.
+  if (!isKnownErrorName(name)) {
     return ErrorName.UNKNOWN_ERROR;
   }
   return name;
 }
 
-function isErrorName(name: string): name is ErrorName {
+function isKnownErrorName(name: string): name is ErrorName {
   return Object.values(ErrorName).includes(name as ErrorName);
 }
