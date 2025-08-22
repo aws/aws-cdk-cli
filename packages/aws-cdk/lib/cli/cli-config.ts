7578eb32369bc9278c2b0ede151a9755f9b70651
Rohan Gupta <44989017+rohang9000@users.noreply.github.com>
2025-08-22 07:14:06 -0700

    removing "requires --from-path" description from cli-config.ts
    Co-authored-by: Ian Hou <45278651+iankhou@users.noreply.github.com>

--- packages/aws-cdk/lib/cli/cli-config.ts
+++ packages/aws-cdk/lib/cli/cli-config.ts
@@ -396,7 +396,7 @@ export async function makeConfig(): Promise<CliConfig> {
           'generate-only': { type: 'boolean', default: false, desc: 'If true, only generates project files, without executing additional operations such as setting up a git repo, installing dependencies or compiling the project' },
           'lib-version': { type: 'string', alias: 'V', default: undefined, desc: 'The version of the CDK library (aws-cdk-lib) to initialize built-in templates with. Defaults to the version that was current when this CLI was built.' },
           'from-path': { type: 'string', desc: 'Path to a local custom template directory or multi-template repository', requiresArg: true, conflicts: ['lib-version'] },
-          'template-path': { type: 'string', desc: 'Path to a specific template within a multi-template repository (requires --from-path)', requiresArg: true },
+          'template-path': { type: 'string', desc: 'Path to a specific template within a multi-template repository', requiresArg: true },
         },
         implies: { 'template-path': 'from-path' },
       },
