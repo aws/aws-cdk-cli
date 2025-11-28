ffa15c2fa5c6002622bd40206d9c7cbc816fcb73
Kaizen Conroy <zen.conroy@gmail.com>
2025-11-28 15:22:44 -0500

    fix errors

--- packages/aws-cdk/lib/cli/telemetry/collect-telemetry.ts
+++ packages/aws-cdk/lib/cli/telemetry/collect-telemetry.ts
@@ -7,7 +7,7 @@ export function canCollectTelemetry(args: any, context: Context): boolean {
   if ((['true', '1'].includes(process.env.CDK_DISABLE_CLI_TELEMETRY ?? '')) ||
     ['false', false].includes(context.get('cli-telemetry')) ||
     (args['version-reporting'] !== undefined && !args['version-reporting']) || /* aliased with telemetry option */
-    (args._[0] === 'cli-telemetry' && args.disable)) /* special case for `cdk cli-telemetry --disable` */ {
+    (Array.isArray(args._) && args._[0] === 'cli-telemetry' && args.disable)) /* special case for `cdk cli-telemetry --disable` */ {
     return false;
   }
 
