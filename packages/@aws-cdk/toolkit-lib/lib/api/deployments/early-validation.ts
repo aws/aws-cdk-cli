465d44bd289bf4ed35055b91cd77ea69d86d343d
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-12-01 10:45:06 +0000

    Integ test

--- packages/@aws-cdk/toolkit-lib/lib/api/deployments/early-validation.ts
+++ packages/@aws-cdk/toolkit-lib/lib/api/deployments/early-validation.ts
@@ -5,10 +5,23 @@ import { ToolkitError } from '../../toolkit/toolkit-error';
 import type { SDK } from '../aws-auth/sdk';
 import type { EnvironmentResources } from '../environment/index';
 
+/**
+ * Reports early validation failures from CloudFormation ChangeSets. To determine what the actual error is
+ * it needs to call the `DescribeEvents` API, and therefore the deployment role in the bootstrap bucket
+ * must have permissions to call that API. This permission was introduced in version 30 of the bootstrap template.
+ * If the bootstrap stack is older than that, a special error message is thrown to instruct the user to
+ * re-bootstrap.
+ */
 export class EarlyValidationReporter implements ValidationReporter {
   constructor(private readonly sdk: SDK, private readonly environmentResources: EnvironmentResources) {
   }
 
+  /**
+   * Checks whether the ChangeSet failed early validation, and if so, throw an error. Otherwise, do nothing.
+   * @param description - the ChangeSet description, resulting from the call to CloudFormation's DescribeChangeSet API
+   * @param changeSetName - the name of the ChangeSet
+   * @param stackName - the name of the stack
+   */
   public async check(description: DescribeChangeSetCommandOutput, changeSetName: string, stackName: string) {
     if (description.Status === ChangeSetStatus.FAILED && description.StatusReason?.includes('AWS::EarlyValidation')) {
       await this.checkBootstrapVersion();
@@ -40,7 +53,7 @@ export class EarlyValidationReporter implements ValidationReporter {
       const env = `aws://${environment.account}/${environment.region}`;
       throw new ToolkitError(
         'While creating the change set, CloudFormation detected errors in the generated templates.\n' +
-        `To see details about these errors, re-bootstrap your environment with 'cdk bootstrap ${env}', and run 'cdk deploy' again.`,
+          `To see details about these errors, re-bootstrap your environment with 'cdk bootstrap ${env}', and run 'cdk deploy' again.`,
       );
     }
   }
