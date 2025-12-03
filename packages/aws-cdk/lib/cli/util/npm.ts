f586c3f8ad27c80088064c2351256b32e5b048c9
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-12-03 15:17:22 +0000

    Revert "execSync"
    This reverts commit a0d886e80153064fbf8e97864498fde2ede39840.

--- packages/aws-cdk/lib/cli/util/npm.ts
+++ packages/aws-cdk/lib/cli/util/npm.ts
@@ -1,21 +1,27 @@
-import { execSync } from 'child_process';
+import { exec as _exec } from 'child_process';
+import { promisify } from 'util';
 import { ToolkitError } from '@aws-cdk/toolkit-lib';
+
+const exec = promisify(_exec);
 
 /* c8 ignore start */
 export async function execNpmView(currentVersion: string) {
   try {
-    const latestResult = execSync('npm view aws-cdk@latest version', { timeout: 3000 }).toString();
-    const currentResult = execSync(`npm view aws-cdk@${currentVersion} name version deprecated --json`, { timeout: 3000 }).toString();
+    // eslint-disable-next-line @cdklabs/promiseall-no-unbounded-parallelism
+    const [latestResult, currentResult] = await Promise.all([
+      exec('npm view aws-cdk@latest version', { timeout: 3000 }),
+      exec(`npm view aws-cdk@${currentVersion} name version deprecated --json`, { timeout: 3000 }),
+    ]);
 
-    if (latestResult && latestResult.trim().length > 0) {
-      throw new ToolkitError(`npm view command for latest version failed: ${latestResult.trim()}`);
+    if (latestResult.stderr && latestResult.stderr.trim().length > 0) {
+      throw new ToolkitError(`npm view command for latest version failed: ${latestResult.stderr.trim()}`);
     }
-    if (currentResult && currentResult.trim().length > 0) {
-      throw new ToolkitError(`npm view command for current version failed: ${currentResult.trim()}`);
+    if (currentResult.stderr && currentResult.stderr.trim().length > 0) {
+      throw new ToolkitError(`npm view command for current version failed: ${currentResult.stderr.trim()}`);
     }
 
-    const latestVersion = latestResult;
-    const currentInfo = JSON.parse(currentResult);
+    const latestVersion = latestResult.stdout;
+    const currentInfo = JSON.parse(currentResult.stdout);
 
     return {
       latestVersion: latestVersion,
