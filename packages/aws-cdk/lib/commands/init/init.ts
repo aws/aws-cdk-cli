fa74ff1c72b3296879b881e825481136e5ff8aef
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-12-03 10:01:23 +0000

    Revert "exit -> close"
    This reverts commit f2bb4c560ad47f21d4660f2ff6ad4fdb099442cf.

--- packages/aws-cdk/lib/commands/init/init.ts
+++ packages/aws-cdk/lib/commands/init/init.ts
@@ -910,7 +910,7 @@ async function execute(ioHelper: IoHelper, cmd: string, args: string[], { cwd }:
   child.stdout.on('data', (chunk) => (stdout += chunk.toString()));
   return new Promise<string>((ok, fail) => {
     child.once('error', (err) => fail(err));
-    child.once('close', (status) => {
+    child.once('exit', (status) => {
       if (status === 0) {
         return ok(stdout);
       } else {
