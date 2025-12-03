ff0164bde16d8fad9bbba0dc5c598bf74455fcb4
Otavio Macedo <288203+otaviomacedo@users.noreply.github.com>
2025-12-03 11:46:42 +0000

    Revert "comment out displayVersionMessage"
    This reverts commit 6ebf4558fab08713d2231b30f58ad97bce8ddebc.

--- packages/aws-cdk/lib/cli/cli.ts
+++ packages/aws-cdk/lib/cli/cli.ts
@@ -5,7 +5,7 @@ import { ToolkitError, Toolkit } from '@aws-cdk/toolkit-lib';
 import * as chalk from 'chalk';
 import { CdkToolkit, AssetBuildTime } from './cdk-toolkit';
 import { ciSystemIsStdErrSafe } from './ci-systems';
-// import { displayVersionMessage } from './display-version';
+import { displayVersionMessage } from './display-version';
 import type { IoMessageLevel } from './io-host';
 import { CliIoHost } from './io-host';
 import { parseCommandLineArguments } from './parse-command-line-arguments';
