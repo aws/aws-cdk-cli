5801b2c51148d105a2bcbaa736336ecb19328514
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-12-10 23:13:09 +0900

    change integ
    integ
    
    integ
    
    integ

--- packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/destroy/cdk-destroy-stage-only.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/cli-integ-tests/destroy/cdk-destroy-stage-only.integtest.ts
@@ -2,18 +2,24 @@ import { DescribeStacksCommand } from '@aws-sdk/client-cloudformation';
 import { integTest, withDefaultFixture } from '../../../lib';
 
 integTest('cdk destroy can destroy stacks in stage-only configuration', withDefaultFixture(async (fixture) => {
+  const integStackSet = 'stage-only';
+
   const stageNameSuffix = 'stage';
   const specifiedStackName = `${stageNameSuffix}/*`;
 
-  await fixture.cdkDeploy(specifiedStackName);
+  await fixture.cdkDeploy(specifiedStackName, {
+    modEnv: {
+      INTEG_STACK_SET: integStackSet,
+    },
+  });
 
   const stackName = `${fixture.fullStackName(stageNameSuffix)}-StackInStage`;
   const stack = await fixture.aws.cloudFormation.send(new DescribeStacksCommand({ StackName: stackName }));
   expect(stack.Stacks?.length ?? 0).toEqual(1);
 
-  await fixture.cdkDestroy('stage/*', {
+  await fixture.cdkDestroy(specifiedStackName, {
     modEnv: {
-      INTEG_STACK_SET: 'stage-only',
+      INTEG_STACK_SET: integStackSet,
     },
   });
 
