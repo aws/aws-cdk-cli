bf1529f0a75518b5d6bb2f7df30726844247cfe8
Rico Huijbers <rix0rrr@gmail.com>
2026-03-26 15:09:23 +0100

    fix: error annotations are not collected
    We collect an "synth failed because of error annotations" error code,
    but the codes for the error annotations themselves are not collected.
    
    Ultimately, these are more important than the "there were errors" code,
    so collect these as well. They are counters, because there can be more
    than one.

--- packages/@aws-cdk/toolkit-lib/lib/toolkit/toolkit.ts
+++ packages/@aws-cdk/toolkit-lib/lib/toolkit/toolkit.ts
@@ -1459,12 +1459,19 @@ function zeroTime(): ElapsedTime {
   return { asMs: 0, asSec: 0 };
 }
 
-function countAssemblyResults(span: IMessageSpan<any>, assembly: cxapi.CloudAssembly) {
+export function countAssemblyResults(span: IMessageSpan<any>, assembly: cxapi.CloudAssembly) {
   const stacksRecursively = assembly.stacksRecursively;
   span.incCounter('stacks', stacksRecursively.length);
   span.incCounter('assemblies', asmCount(assembly));
   span.incCounter('errorAnns', sum(stacksRecursively.map(s => s.messages.filter(m => m.level === SynthesisMessageLevel.ERROR).length)));
   span.incCounter('warnings', sum(stacksRecursively.map(s => s.messages.filter(m => m.level === SynthesisMessageLevel.WARNING).length)));
+
+  const annotationErrorCodes = stacksRecursively
+    .flatMap(s => Object.values(s.metadata)
+      .flatMap(ms => ms.filter(m => m.type === ANNOTATION_ERROR_CODE_TYPE)));
+  for (const annotationErrorCode of annotationErrorCodes) {
+    span.incCounter(`errorAnn:${annotationErrorCode.data}`);
+  }
 
   function asmCount(x: cxapi.CloudAssembly): number {
     return 1 + x.nestedAssemblies.reduce((acc, asm) => acc + asmCount(asm.nestedAssembly), 0);
@@ -1474,3 +1481,5 @@ function countAssemblyResults(span: IMessageSpan<any>, assembly: cxapi.CloudAsse
 function sum(xs: number[]) {
   return xs.reduce((a, b) => a + b, 0);
 }
+
+const ANNOTATION_ERROR_CODE_TYPE = 'aws:cdk:error-code';
