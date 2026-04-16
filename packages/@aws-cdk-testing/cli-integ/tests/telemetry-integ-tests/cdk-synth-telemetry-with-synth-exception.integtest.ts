6852bd6a4e9e8c7aebcb1e0986517e6ab09e69fc
Rico Huijbers <rix0rrr@gmail.com>
2026-04-16 13:04:50 +0200

    fix: error information is lost
    Error code information was attached to a `ToolkitError` that was later
    on rethrown with a blanket code, losing the error code. Maintain the
    error code if we have it.
    
    Also add an integ test to confirm we correctly extract synthesis errors
    from an application that fails with a synthesis exception.

---
+++ packages/@aws-cdk-testing/cli-integ/tests/telemetry-integ-tests/cdk-synth-telemetry-with-synth-exception.integtest.ts
@@ -0,0 +1,34 @@
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import { integTest, withSpecificFixture } from '../../lib';
+
+integTest(
+  'cdk synth with telemetry and validation error leads to invoke failure',
+  withSpecificFixture('app-w-synthesis-error', async (fixture) => {
+    const telemetryFile = path.join(fixture.integTestDir, `telemetry-${Date.now()}.json`);
+    const output = await fixture.cdk(['synth', `--telemetry-file=${telemetryFile}`], {
+      allowErrExit: true,
+      verboseLevel: 3, // trace mode
+    });
+
+    expect(output).toContain('This is an error');
+
+    // Check the trace that telemetry was executed successfully despite error in synth
+    expect(output).toContain('Telemetry Sent Successfully');
+
+    const json = fs.readJSONSync(telemetryFile);
+    expect(json).toEqual([
+      expect.objectContaining({
+        event: expect.objectContaining({
+          state: 'FAILED',
+          eventType: 'SYNTH',
+        }),
+        error: {
+          name: 'synth:InvalidBucketNameValue',
+        },
+      }),
+    ]);
+    fs.unlinkSync(telemetryFile);
+  }),
+);
+
