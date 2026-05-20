a221cab9d5cd0f89800bca710bdf9f6d54e19017
Rico Huijbers <rix0rrr@gmail.com>
2026-05-20 15:15:25 +0200

    --save-dev doesn't exhibit the problems we expect

--- packages/@aws-cdk-testing/cli-integ/tests/init-typescript-app/init-typescript-app.integtest.ts
+++ packages/@aws-cdk-testing/cli-integ/tests/init-typescript-app/init-typescript-app.integtest.ts
@@ -39,7 +39,6 @@ TYPESCRIPT_VERSIONS.forEach(tsVersion => {
     await shell.shell(['npm', '--version']);
 
     await shell.shell(['cdk', 'init', '--lib-version', context.library.requestedVersion(), '-l', 'typescript', 'app', '--generate-only']);
-    await shell.shell(['npm', 'ci']); // this will fail if we have bundled dependencies that introduce version conflicts
 
     // Necessary because recent versions of ts-jest require TypeScript>=4.3 but we
     // still want to test with older versions as well.
