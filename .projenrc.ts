cb1a52d8166b82e2e7603e3de93a08c7284c8431
Rico Huijbers <rix0rrr@gmail.com>
2026-06-22 17:07:38 +0200

    chore(integ-runner): remove CLI as run-time dependency
    This is probably not necessary anymore.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1495,7 +1495,7 @@ const integRunner = configureProject(
     deps: [
       cloudAssemblySchema.customizeReference({ versionType: 'any-future' }),
       cloudAssemblyApi.customizeReference({ versionType: 'exact' }),
-      cli.customizeReference({ versionType: 'exact' }),
+      // cli.customizeReference({ versionType: 'exact' }),
       cdkAssetsLib.customizeReference({ versionType: 'exact' }),
       cloudFormationDiff.customizeReference({ versionType: 'exact' }),
       toolkitLib.customizeReference({ versionType: 'exact' }),
