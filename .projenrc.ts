b01d7d50ade51f4464f405047beca45b23fbdd90
Momo Kornher <kornherm@amazon.co.uk>
2026-09-04 12:11:00 +0100

    chore(cli): generate toolkit-versions.zip artifact for docs publishing
    Adds a `versions` task to the aws-cdk package that generates a
    versions.json describing the toolkit package versions of the current
    build and zips it into dist/toolkit-versions.zip, which the release
    workflow publishes to the docs bucket.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1467,11 +1467,22 @@ for (const tsconfig of [cli.tsconfig, cli.tsconfigDev]) {
 }
 
 // Publishing Toolkit CLI version
+// We don't actually publish CLI docs here, but we can collect some interesting version information
+const cliVersionsTask = cli.addTask('versions', {
+  exec: [
+    'tsx --tsconfig test/tsconfig.json scripts/gen-versions-json.ts',
+    'rm -f dist/toolkit-versions.zip',
+    'zip -j -q dist/toolkit-versions.zip dist/versions.json',
+  ].join(' && '),
+});
+cli.packageTask.spawn(cliVersionsTask);
+
 new S3DocsPublishing(cli, {
   docsStream: 'CLI',
-  artifactPath: 'api-extractor-docs.zip',
+  artifactPath: 'toolkit-versions.zip',
   bucketName: '${{ vars.DOCS_BUCKET_NAME }}',
   roleToAssume: '${{ vars.PUBLISH_CLI_VERSION_ROLE_ARN }}',
+  s3PathPrefix: 'toolkit-versions',
 });
 
 // #endregion
